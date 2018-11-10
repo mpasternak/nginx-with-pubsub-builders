2f737ff7fc4b47db9a91d59b86f97a3e436493f0
Michał Pasternak <michal.dtz@gmail.com>
2018-11-10 22:22:55 +0100

    Cosmic

---
+++ cosmic64/provisioning/build-user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export NGINX_VERSION=1.14.1
+export VERSION_SUFFIX=0+cosmic0+pushstream0
+export UBUNTU_VERSION=18.10
+export PKGNAME=nginx_$NGINX_VERSION-$VERSION_SUFFIX\_source.changes
+
+patch -p0 < /nginx-$NGINX_VERSION-ubuntu-$UBUNTU_VERSION.patch
+
+cd nginx-$NGINX_VERSION
+
+debuild -S -sa
+
+cd ..
+
+ls -las
+
+debsign $PKGNAME
+dput ppa:dotz/nginx-with-push-stream-module $PKGNAME
