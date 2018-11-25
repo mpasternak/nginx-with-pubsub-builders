58b0a463bffc88990c28218ac554790e9ca45b9d
Michał Pasternak <michal.dtz@gmail.com>
2018-11-25 11:36:55 +0100

    Move git repo cloning to build script

--- bionic64/provisioning/build-user.sh
+++ bionic64/provisioning/build-user.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 export NGINX_VERSION=1.14.1
-export VERSION_SUFFIX=0+bionic0+pushstream0
+export VERSION_SUFFIX=0+bionic0+pushstream1
 export UBUNTU_VERSION=18.04
 export PKGNAME=nginx_$NGINX_VERSION-$VERSION_SUFFIX\_source.changes
+
+git clone https://github.com/wandenberg/nginx-push-stream-module.git
+
+mv nginx-push-stream-module nginx-$NGINX_VERSION/contrib
 
 patch -p0 < /nginx-$NGINX_VERSION-ubuntu-$UBUNTU_VERSION.patch
 
 cd nginx-$NGINX_VERSION
+
+dpkg-source --commit
 
 debuild -S -sa
 
--- trusty64/provisioning/build-user.sh
+++ trusty64/provisioning/build-user.sh
@@ -2,12 +2,18 @@
 
 export NGINX_VERSION=1.12.2
 export UBUNTU_VERSION=14.04
-export VERSION_SUFFIX=0+trusty0+pushstream0
+export VERSION_SUFFIX=0+trusty0+pushstream1
 export PKGNAME=nginx_$NGINX_VERSION-$VERSION_SUFFIX\_source.changes
+
+git clone https://github.com/wandenberg/nginx-push-stream-module.git
+
+mv nginx-push-stream-module nginx-$NGINX_VERSION/contrib
 
 patch -p0 < /nginx-$NGINX_VERSION-ubuntu-$UBUNTU_VERSION.patch
 
 cd nginx-$NGINX_VERSION
+
+dpkg-source --commit
 
 debuild -S -sa
 
